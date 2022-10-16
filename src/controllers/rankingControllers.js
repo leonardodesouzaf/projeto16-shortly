e86247f3cb0b17499f15cb92602aa952721c4516
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-10-16 19:17:51 -0300

    ranking done

--- src/controllers/rankingControllers.js
+++ src/controllers/rankingControllers.js
@@ -2,11 +2,13 @@ import connection from '../database/database.js';
 
 export async function getRanking (req, res){
     try {
-        const users = await connection.query(`SELECT * FROM users;`);
-        const urls = await connection.query(`SELECT * FROM urls ORDER BY ;`);
-        let response = [];
-
-        return res.status(200).send(response);
+        const ranking = await connection.query(`SELECT users.id,users.name,COUNT (urls.id) AS "linksCount",SUM(urls."visitCount") AS "visitCount" FROM users LEFT JOIN urls ON users.id=urls."userId" GROUP BY users.id ORDER BY SUM(urls."visitCount") DESC NULLS LAST LIMIT 10;`);
+        for(let i=0; i<ranking.rows.length; i++){
+            if(ranking.rows[i].visitCount === null){
+                ranking.rows[i].visitCount = '0';
+            }
+        }
+        return res.status(200).send(ranking.rows);
     } catch (error) {
         return res.status(500).send('Não foi possível conectar ao servidor!');
     }
